b7ae6d2f54247cc115727476c57e1048f55a1932
RENDY CIANG <rendy.ciang@binus.edu>
2025-04-01 10:00:45 +0700

    fix: check auth

--- Server/src/controllers/AuthController.ts
+++ Server/src/controllers/AuthController.ts
@@ -16,7 +16,7 @@ const Register: RequestHandler = async (request, response, next) => {
     const validatedData = validateRegister.parse(request.body);
     const { role, email, phone, password } = validatedData;
 
-    // Cek 
+    // Cek Baru
     const existingUser = await prisma.user.findUnique({
       where: {
         email,
