c771b191315f4eb38b303bdb100136e80039418d
RENDY CIANG <rendy.ciang@binus.edu>
2025-05-21 00:18:18 +0700

    fix archived

--- Server/src/routes/menuRouter.ts
+++ Server/src/routes/menuRouter.ts
@@ -12,6 +12,7 @@ menuRouter.get(
   checkRole(["Admin", "Seller"]),
   MenuController.vendorMenuList
 );
+// archived menu
 menuRouter.get("/get-archived-menu", MenuController.getIsArchived);
 menuRouter.get("/get-menu/:id", MenuController.getMenuById);
 menuRouter.post(
