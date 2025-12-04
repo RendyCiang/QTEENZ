84f73564700d1c96caa5a15979dfd575479a09b7
MikeKomari <michaelk.projects@gmail.com>
2025-12-04 17:20:35 +0700

    oops

--- Client/src/utils/API.ts
+++ Client/src/utils/API.ts
@@ -1,19 +1,19 @@
 import axios from "axios";
 
 export const API = axios.create({
-  // baseURL: "https://qteenz-api.vercel.app/api",
-  baseURL: "http://localhost:8000/api",
+	baseURL: "https://qteenz-api.vercel.app/api",
+	// baseURL: "http://localhost:8000/api",
 });
 
 API.interceptors.request.use(
-  (request) => {
-    const token =
-      sessionStorage.getItem("token") || localStorage.getItem("token");
-    if (token) {
-      request.headers.Authorization = `Bearer ${token}`;
-    }
+	(request) => {
+		const token =
+			sessionStorage.getItem("token") || localStorage.getItem("token");
+		if (token) {
+			request.headers.Authorization = `Bearer ${token}`;
+		}
 
-    return request;
-  },
-  () => Promise.reject()
+		return request;
+	},
+	() => Promise.reject()
 );
